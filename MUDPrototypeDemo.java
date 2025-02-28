abc6aec9f72f431403e76beadbe04981212abc34
AbievBauyrzhan <rayderandaxus@gmail.com>
2025-02-28 08:43:30 +0500

    Delete MUDPrototypeDemo.java

--- MUDPrototypeDemo.java
+++
@@ -1,8 +0,0 @@
-public class MUDPrototypeDemo {
-    public static void main(String[] args) {
-        Room originalRoom = new Room("Treasure Room");
-        Room clonedRoom = (Room) originalRoom.cloneEntity();
-        System.out.println("Original Room:" + originalRoom.getName());
-        System.out.println("Cloned Room:" + clonedRoom.getName());
-    }
-}
