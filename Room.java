2b0ec20a6612a0de5c6d2a40a7ece35ef152b03b
AbievBauyrzhan <rayderandaxus@gmail.com>
2025-02-28 08:43:50 +0500

    Delete Room.java

--- Room.java
+++
@@ -1,8 +0,0 @@
-public class Room implements ICloneableGameEntity {
-    private String name;
-    public Room(String name) { this.name = name; }
-    public String getName() { return name; }
-    public ICloneableGameEntity cloneEntity() { return new Room(this.name );
-    }
-}
-
